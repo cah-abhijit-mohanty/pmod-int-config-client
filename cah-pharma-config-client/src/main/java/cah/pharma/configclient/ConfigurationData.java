69aea0766a1234f68c72cd43c12ca069f19611df
cah-abhijit-mohanty <51443476+cah-abhijit-mohanty@users.noreply.github.com>
2019-06-27 10:36:41 +0530

    Adding few more changes

--- cah-pharma-config-client/src/main/java/cah/pharma/configclient/ConfigurationData.java
+++ cah-pharma-config-client/src/main/java/cah/pharma/configclient/ConfigurationData.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Data
 @Component
-@ConfigurationProperties ( "serverdata" )
+@ConfigurationProperties ("serverdata")
 public  class  ConfigurationData {	
 	private  int minimum;
 	private  int maximum;
